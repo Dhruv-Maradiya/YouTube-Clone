5d3e2b77fc65327dfe3516d7af55a9c4231a6086
Dhruv <dhruvmaradiya0@gmail.com>
2021-07-26 19:30:02 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./css/Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <h1>hi i am header</h1>
+        </div>
+    )
+}
+
+export default Header;
