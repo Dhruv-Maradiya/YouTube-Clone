c8ed37d2b9abf836bf5b10eefc5d00442e421af1
Dhruv <dhruvmaradiya0@gmail.com>
2021-07-26 20:02:50 +0530

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../css/App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>hello Dhruv</h1>
+      <Header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import "./css/Header.css";
+import "../css/Header.css";
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { Avatar } from '@material-ui/core';
 
 const Header = () => {
     return (
-        <div>
-            <h1>hi i am header</h1>
+        <div className="header">
+            <div className="header__left">
+                <MenuIcon />
+                <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_(2015-2017).svg/1280px-Logo_of_YouTube_(2015-2017).svg.png" alt="logo" />
+            </div>
+            <div className="header__input">
+                <input type="text" />
+                <SearchIcon />
+            </div>
+            <div className="header__icons">
+                <VideoCallIcon />
+                <AppsIcon />
+                <NotificationsIcon />
+                <Avatar />
+            </div>
         </div>
     )
 }
