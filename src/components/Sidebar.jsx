92cb3a335ced3a8d892205f30253ccf6914a7208
Dhruv <dhruvmaradiya0@gmail.com>
2021-07-27 14:11:28 +0530

    added sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/Sidebar.css";
+
+const Sidebar = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Sidebar;
