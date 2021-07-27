9e373e2825d986c12c75c3ea66eb6e1b004cad7b
Dhruv <dhruvmaradiya0@gmail.com>
2021-07-27 10:24:58 +0530

    css file responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,32 @@ import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="header__left">
-                <MenuIcon />
+                <IconButton>
+                    <MenuIcon className="header__left__icon" />
+                </IconButton>
                 <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_(2015-2017).svg/1280px-Logo_of_YouTube_(2015-2017).svg.png" alt="logo" />
             </div>
             <div className="header__input">
-                <input type="text" />
-                <SearchIcon />
+                <input type="text" placeholder="Search" />
+                <SearchIcon className="header__inputButton" />
             </div>
             <div className="header__icons">
-                <VideoCallIcon />
-                <AppsIcon />
-                <NotificationsIcon />
-                <Avatar />
+                <IconButton className="header__iconButton" >
+                    <VideoCallIcon className="header__icon" />
+                </IconButton>
+                <IconButton className="header__iconButton" >
+                    <AppsIcon className="header__icon" />
+                </IconButton>
+                <IconButton className="header__iconButton" >
+                    <NotificationsIcon className="header__icon" />
+                </IconButton>
+                <Avatar className="header__icon header__Avatar" />
             </div>
         </div>
     )
