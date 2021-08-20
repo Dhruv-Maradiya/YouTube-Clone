51563eb46d031e371ece9c90f2c9d7ce675b3b13
Dhruv <dhruvmaradiya0@gmail.com>
2021-08-20 19:02:13 +0530

    on click of related video to video player

--- src/components/RelatedVideo.jsx
+++ src/components/RelatedVideo.jsx
@@ -1,12 +1,14 @@
 import moment from 'moment';
 import numeral from 'numeral';
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import request from '../api';
 import "../css/RelatedVideo/RelatedVideo.css";
 
 const RelatedVideo = ({ item }) => {
     const [views, setViews] = useState(0);
     const [duration, setDuration] = useState(0);
+    const history = useHistory();
     useEffect(() => {
         const getChannelDetails = async (videoId) => {
             const { data: { items } } = await request('/videos', {
@@ -22,13 +24,16 @@ const RelatedVideo = ({ item }) => {
         }
         getChannelDetails(item.id.videoId);
     }, [item.id.videoId]);
+    const playVideo = (videoId) => {
+        history.push(`/watch/${videoId}`);
+    }
     if (item !== undefined && item !== null && item.snippet !== undefined) {
         var { snippet: { publishedAt, title, channelTitle, thumbnails: { medium: { url } } } } = item;
     } else {
         return null;
     }
     return (
-        <div className="relatedVideo">
+        <div className="relatedVideo" onClick={() => { playVideo(item.id.videoId) }}>
             <div className="relatedVideo__thumbnail">
                 <img src={url} alt="" />
                 <span>{moment.utc(moment.duration(duration).asSeconds() * 1000).format("mm:ss")}</span>
